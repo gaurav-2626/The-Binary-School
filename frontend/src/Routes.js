b90579c7185c9ff80f068656d647bb879f213218
Gaurav Agrawal <gauravagrawal_@outlook.com>
2021-02-11 16:55:19 +0530

    First change

---
+++ frontend/src/Routes.js
@@ -0,0 +1,22 @@
+//react router
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
+
+//pages
+import Home from "./pages/home/home.js"
+
+
+
+
+
+const Routes = () => {
+
+    return (
+      <Router>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+          </Switch>
+      </Router>
+    );
+  }
+  
+  export default Routes;
