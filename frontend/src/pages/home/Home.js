d0ce8a1b5d16ff82000e1c842ba443923f0b93b0
sambhavjai <38015768+sambhavjai@users.noreply.github.com>
2021-03-10 22:12:16 +0530

    added component5

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -9,6 +9,7 @@ import Header from "../../core/Header.js";
 import Component1 from "./components/Component1.js";
 import Component2 from "./components/Component2.js";
 import Component3 from "./components/Component3.js";
+import Component5 from "./components/Component5.js";
 
 const Home = () => {
   return (
@@ -17,6 +18,7 @@ const Home = () => {
       <Component1 />
       <Component2 />
       <Component3 />
+      <Component5 />
       <Footer />
     </div>
   );
